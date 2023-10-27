61ef97b610828f21b29545ee4e28e7f544f74f1c
AbdulAleem Alsayed <abada400022@gmail.com>
2023-10-27 20:31:44 +0200

    make it better

--- stopwatch_ui.py
+++ stopwatch_ui.py
@@ -16,17 +16,12 @@ class StopwatchUI:
         self.create_labels()
         self.create_buttons()
         self.create_textfield()
+        self.create_text_list()
         self.update_time()
 
     def create_labels(self):
         self.time_label = tk.Label(self.window, text="00:00:00", font=("Arial", 40))
         self.time_label.pack()
-        self.start_time_display = tk.Label(self.window, text="Start Time: ", font=("Arial", 12))
-        self.start_time_display.pack()
-        self.stop_time_display = tk.Label(self.window, text="Stop Time: ", font=("Arial", 12))
-        self.stop_time_display.pack()
-        self.title_label = tk.Label(self.window, text="Title: ", font=("Arial", 12))
-        self.title_label.pack()
 
     def create_buttons(self):
         start_button = tk.Button(self.window, text="Start", command=self.start_stopwatch)
@@ -34,13 +29,14 @@ class StopwatchUI:
         restart_button = tk.Button(self.window, text="Restart", command=self.restart_stopwatch)
 
         title_label = tk.Label(self.window, text="Title:", font=("Arial", 12))
-        self.title_entry = tk.Entry(self.window, font=("Arial", 12))
+        self.title_entry = tk.Entry(self.window, font=("Arial", 12))  # Add this line
         title_label.pack()
         self.title_entry.pack()
 
         start_button.pack()
         pause_button.pack()
         restart_button.pack()
+
 
     def create_textfield(self):
         dialog_interval_label = tk.Label(self.window, text="Dialog Interval:", font=("Arial", 12))
@@ -61,6 +57,10 @@ class StopwatchUI:
 
         self.dialog_interval_entry.bind("<Return>", self.save_interval)
 
+    def create_text_list(self):
+        self.text_list = tk.Listbox(self.window, font=("Arial", 12), width=50, height=10)
+        self.text_list.pack()
+
     def start_stopwatch(self):
         self.stopwatch.start()
         self.update_time()
@@ -70,21 +70,22 @@ class StopwatchUI:
         start_time = self.stopwatch.start_times[-1]
         stop_time = datetime.now()
         self.stopwatch.add_title(title, start_time, stop_time)
-
+        self.display_titles()  # Display the updated list of titles
         self.stopwatch.pause()
         self.update_time()
         self.last_dialog_time = datetime.now()
-        self.title_label.config(text=f"Title: {title}")
 
     def restart_stopwatch(self):
         if self.stopwatch.running:
             self.stopwatch.reset()
             self.update_time()
-    # Add a method to display the titles
+
     def display_titles(self):
+        self.text_list.delete(0, tk.END)  # Clear the existing list
         titles = self.stopwatch.get_titles()
-        title_text = "\n".join([f"{title}: {start} - {stop}" for title, start, stop in titles])
-        self.title_label.config(text=f"Titles:\n{title_text}")
+        for title, start, stop in titles:
+            formatted_text = f"{title}: {self.format_datetime([start])} - {self.format_datetime([stop])}"
+            self.text_list.insert(tk.END, formatted_text)
 
     def check_resume_timer(self):
         interval = read_dialog_interval()
@@ -97,7 +98,7 @@ class StopwatchUI:
                 self.update_time()
             self.last_dialog_time = datetime.now()
 
-        self.window.after(1000, self.check_resume_timer)  # Start checking after the first pause
+        self.window.after(1000, self.check_resume_timer)
 
     def update_time(self):
         current_time = datetime.now()
@@ -108,17 +109,8 @@ class StopwatchUI:
 
         total_elapsed = elapsed_time + sum(self.stopwatch.elapsed_times(), timedelta())
         time_str = self.format_timedelta(total_elapsed)
-
         self.time_label.config(text=time_str)
-
-
-        # Display titles alongside their respective start and end times
-        titles = self.stopwatch.get_titles()
-        titles_str = "\n".join([f"{title}: {self.format_datetime([start])} - {self.format_datetime([stop])}" for title, start, stop in titles])
-        self.title_label.config(text=f"Titles:\n{titles_str}")
-
         self.time_label.after(1000, self.update_time)
-
 
     def format_datetime(self, dt_list):
         if not dt_list:
