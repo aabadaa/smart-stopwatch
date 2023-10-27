0128cebd2fd3b4a9b1d67884e0f93d1b201d6c8a
AbdulAleem Alsayed <abada400022@gmail.com>
2023-10-27 20:22:34 +0200

    add title to each start,stop

--- stopwatch.py
+++ stopwatch.py
@@ -2,31 +2,38 @@ from datetime import datetime, timedelta
 
 class Stopwatch:
     def __init__(self):
+        self.running = False
+        self.paused = False
         self.start_times = []
         self.stop_times = []
-        self.running = False
-        self.paused = False  # Add a 'paused' attribute
+        self.titles = []  # List to store titles and their respective start and end times
 
     def start(self):
         if not self.running:
             self.start_times.append(datetime.now())
             self.running = True
-            self.paused = False  # Reset 'paused' when starting
 
     def pause(self):
         if self.running:
             self.stop_times.append(datetime.now())
             self.running = False
-            self.paused = True  # Set 'paused' to True when pausing
+            self.paused = True
 
     def reset(self):
+        self.running = False
+        self.paused = False
         self.start_times = []
         self.stop_times = []
-        self.running = False
-        self.paused = False  # Reset 'paused' when resetting
+        self.titles = []  # Clear the titles when resetting
 
     def elapsed_times(self):
-        elapsed_times = []
-        for start_time, stop_time in zip(self.start_times, self.stop_times):
-            elapsed_times.append(stop_time - start_time)
-        return elapsed_times
+        elapsed = []
+        for start, stop in zip(self.start_times, self.stop_times):
+            elapsed.append(stop - start)
+        return elapsed
+
+    def add_title(self, title, start_time, stop_time):
+        self.titles.append((title, start_time, stop_time))
+
+    def get_titles(self):
+        return self.titles
--- stopwatch_ui.py
+++ stopwatch_ui.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import messagebox, simpledialog
 from datetime import datetime, timedelta
 from stopwatch import Stopwatch
 from fileUtils import *
@@ -26,21 +25,40 @@ class StopwatchUI:
         self.start_time_display.pack()
         self.stop_time_display = tk.Label(self.window, text="Stop Time: ", font=("Arial", 12))
         self.stop_time_display.pack()
+        self.title_label = tk.Label(self.window, text="Title: ", font=("Arial", 12))
+        self.title_label.pack()
 
     def create_buttons(self):
         start_button = tk.Button(self.window, text="Start", command=self.start_stopwatch)
         pause_button = tk.Button(self.window, text="Pause", command=self.pause_stopwatch)
         restart_button = tk.Button(self.window, text="Restart", command=self.restart_stopwatch)
+
+        title_label = tk.Label(self.window, text="Title:", font=("Arial", 12))
+        self.title_entry = tk.Entry(self.window, font=("Arial", 12))
+        title_label.pack()
+        self.title_entry.pack()
+
         start_button.pack()
         pause_button.pack()
         restart_button.pack()
 
     def create_textfield(self):
-        dialog_interval_label = tk.Label(self.window, text="Dialog Interval (minutes):", font=("Arial", 12))
+        dialog_interval_label = tk.Label(self.window, text="Dialog Interval:", font=("Arial", 12))
         dialog_interval_label.pack()
+
         self.dialog_interval_entry = tk.Entry(self.window, font=("Arial", 12))
         self.dialog_interval_entry.insert(0, read_dialog_interval())
         self.dialog_interval_entry.pack()
+
+        self.unit_selection = tk.StringVar()
+        self.unit_selection.set("minutes")  # Set a default value
+        units_label = tk.Label(self.window, text="Units:", font=("Arial", 12))
+        units_label.pack()
+        minutes_radio = tk.Radiobutton(self.window, text="Minutes", variable=self.unit_selection, value="minutes")
+        seconds_radio = tk.Radiobutton(self.window, text="Seconds", variable=self.unit_selection, value="seconds")
+        minutes_radio.pack()
+        seconds_radio.pack()
+
         self.dialog_interval_entry.bind("<Return>", self.save_interval)
 
     def start_stopwatch(self):
@@ -48,16 +66,25 @@ class StopwatchUI:
         self.update_time()
 
     def pause_stopwatch(self):
+        title = self.title_entry.get()
+        start_time = self.stopwatch.start_times[-1]
+        stop_time = datetime.now()
+        self.stopwatch.add_title(title, start_time, stop_time)
+
         self.stopwatch.pause()
         self.update_time()
         self.last_dialog_time = datetime.now()
+        self.title_label.config(text=f"Title: {title}")
 
     def restart_stopwatch(self):
         if self.stopwatch.running:
-            response = messagebox.askyesno("Confirm Restart", "Are you sure you want to restart the stopwatch?")
-            if response:
-                self.stopwatch.reset()
-                self.update_time()
+            self.stopwatch.reset()
+            self.update_time()
+    # Add a method to display the titles
+    def display_titles(self):
+        titles = self.stopwatch.get_titles()
+        title_text = "\n".join([f"{title}: {start} - {stop}" for title, start, stop in titles])
+        self.title_label.config(text=f"Titles:\n{title_text}")
 
     def check_resume_timer(self):
         interval = read_dialog_interval()
@@ -83,10 +110,15 @@ class StopwatchUI:
         time_str = self.format_timedelta(total_elapsed)
 
         self.time_label.config(text=time_str)
-        self.start_time_display.config(text=f"Start Time: {self.format_datetime(self.stopwatch.start_times)}")
-        self.stop_time_display.config(text=f"Stop Time: {self.format_datetime(self.stopwatch.stop_times)}")
+
+
+        # Display titles alongside their respective start and end times
+        titles = self.stopwatch.get_titles()
+        titles_str = "\n".join([f"{title}: {self.format_datetime([start])} - {self.format_datetime([stop])}" for title, start, stop in titles])
+        self.title_label.config(text=f"Titles:\n{titles_str}")
 
         self.time_label.after(1000, self.update_time)
+
 
     def format_datetime(self, dt_list):
         if not dt_list:
